566ae0dce1bee7e45c3e847a4fed84d23792d1cc
Andrei Bespalov <Andrei_Bespalov@epam.com>
2018-02-05 19:06:05 +0300

    Adding Hadoop homework1

---
+++ module1_homework1/MR_code_v2/src/main/java/training/bigdata/epam/WordAnalyzer.java
@@ -0,0 +1,91 @@
+package training.bigdata.epam;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.*;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WordAnalyzer {
+
+    public static final Logger logger = LoggerFactory.getLogger(WordAnalyzer.class);
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, IntWritable, Text> {
+
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(new IntWritable(word.toString().length()), word);
+            }
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<IntWritable, Text, IntWritable, Text> {
+
+        public void reduce(IntWritable key, Iterable<Text> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+
+            Set<String> words = new HashSet<>();
+            for (Text val : values) {
+                words.add(val.toString());
+            }
+            context.write(key, new Text(String.join(", ", words)));
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word analyzer");
+        job.setJarByClass(WordAnalyzer.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setSortComparatorClass(IntComparator.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+    }
+
+
+    public static class IntComparator extends WritableComparator {
+
+        public IntComparator() {
+            super(IntWritable.class);
+        }
+
+        @Override
+        public int compare(byte[] b1, int s1, int l1,
+                           byte[] b2, int s2, int l2) {
+
+            Integer v1 = ByteBuffer.wrap(b1, s1, l1).getInt();
+            Integer v2 = ByteBuffer.wrap(b2, s2, l2).getInt();
+
+            return v1.compareTo(v2) * (-1);
+        }
+    }
+
+
+
+}
---
+++ module1_homework1/MR_code_v2/src/test/java/training/bigdata/epam/WordAnalyzerTest.java
@@ -0,0 +1,88 @@
+package training.bigdata.epam;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.hadoop.io.*;
+import org.junit.*;
+
+import org.apache.hadoop.mrunit.types.Pair;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.apache.tools.ant.types.Assertions;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import training.bigdata.epam.WordAnalyzer;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class WordAnalyzerTest {
+            MapDriver<Object, Text, Text, MapWritable> mapDriver;
+            ReduceDriver<Text, MapWritable, Text, IntWritable> reduceDriver;
+            MapReduceDriver<Object, Text, Text, MapWritable, Text, IntWritable> mapReduceDriver;
+
+
+        @Before
+        public void setUp() {
+            WordAnalyzer.TokenizerMapper mapper = new WordAnalyzer.TokenizerMapper();
+            WordAnalyzer.IntSumReducer reducer = new WordAnalyzer.IntSumReducer();
+            mapDriver = MapDriver.newMapDriver(mapper);
+            reduceDriver = ReduceDriver.newReduceDriver(reducer);
+            mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer);
+        }
+
+
+        @Test
+        public void testMapper() throws IOException {
+            mapDriver.withInput(new Text("key"), new Text("12345"));
+            List<Pair<Text,MapWritable>> results = new ArrayList<Pair<Text,MapWritable>>();
+            results = mapDriver.run();
+            Map.Entry<Writable, Writable> entry = results.get(0).getSecond().entrySet().iterator().next();
+
+            assertEquals ("key",results.get(0).getFirst().toString());
+            assertEquals (new Text("12345"), (Text) entry.getKey());
+            assertEquals (new IntWritable(5), (IntWritable) entry.getValue());
+        }
+
+
+        @Test
+        public void testReducer() throws IOException {
+            List<MapWritable> values = new ArrayList<MapWritable>();
+
+            MapWritable mapRecord1 = new MapWritable();
+            mapRecord1.put(new Text("1"), new IntWritable(1));
+            mapRecord1.put(new Text("12"), new IntWritable(2));
+
+            MapWritable mapRecord2 = new MapWritable();
+            mapRecord2.put(new Text("123"), new IntWritable(3));
+            mapRecord2.put(new Text("1234"), new IntWritable(4));
+            mapRecord2.put(new Text("12345"), new IntWritable(5));
+
+            values.add(mapRecord1);
+            values.add(mapRecord2);
+
+            reduceDriver.withInput(new Text("key"), values);
+            reduceDriver.withOutput(new Text("12345"), new IntWritable(5));
+            reduceDriver.runTest();
+        }
+
+        @Test
+        public void testMapReduce() throws IOException {
+            mapReduceDriver.withInput(new Text("key"), new Text("1 12 123 1234 12345"));
+            List<Pair<Text,IntWritable>> results = new ArrayList<Pair<Text,IntWritable>>();
+            results = mapReduceDriver.run();
+
+            assertEquals ("12345",results.get(0).getFirst().toString());
+            assertEquals (new IntWritable(5),results.get(0).getSecond());
+        }
+    }
+
+
+
+
