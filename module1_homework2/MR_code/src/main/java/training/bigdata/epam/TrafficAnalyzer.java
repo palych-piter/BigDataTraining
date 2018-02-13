78f7dedd6bd6d521f8a815eac213c0e5798b6cc3
Andrei Bespalov <Andrei_Bespalov@epam.com>
2018-02-13 18:45:16 +0300

    Working version without Combiner

--- module1_homework2/MR_code/src/main/java/training/bigdata/epam/TrafficAnalyzer.java
+++ module1_homework2/MR_code/src/main/java/training/bigdata/epam/TrafficAnalyzer.java
@@ -34,16 +34,14 @@ public class TrafficAnalyzer {
             float floatBytes ;
             String ip = new String();
             String inputString = new String(value.toString());
-            MapWritable outputMapRecord = new MapWritable();
 
             Scanner scanner = new Scanner(inputString);
             while (scanner.hasNextLine()) {
 
                 String line = scanner.nextLine();
                 String[] splitLine = line.split(" ");
-
                 ip = splitLine[0];
-                floatBytes = bytes.equals("-")?0:Float.parseFloat(splitLine[9]);
+                floatBytes = splitLine[9].equals("-")?0:Float.parseFloat(splitLine[9]);
 
                 UserAgent userAgent = UserAgent.parseUserAgentString(line);
 
@@ -55,55 +53,52 @@ public class TrafficAnalyzer {
     }
 
 
-    public static class Combiner
-            extends Reducer<Text, FloatWritable, Text, MapWritable> {
+//    public static class Combiner
+//            extends Reducer<Text, FloatWritable, Text, MapWritable> {
+//
+//        public void reduce(Text key, Iterable<FloatWritable> byteValues,
+//                           Context context
+//        ) throws IOException, InterruptedException {
+//
+//            MapWritable outputMapRecord = new MapWritable();
+//            float totalBytes = 0;
+//            int counter = 0;
+//            double avarageBytes;
+//
+//            for (FloatWritable bytes : byteValues) {
+//                totalBytes = totalBytes + Float.parseFloat(bytes.toString());
+//                counter++;
+//            }
+//
+//            avarageBytes = (double) totalBytes / (double) counter;
+//
+//            outputMapRecord.put(new Text("total"), new DoubleWritable(totalBytes));
+//            outputMapRecord.put(new Text("average"), new DoubleWritable(avarageBytes));
+//
+//            context.write(key, outputMapRecord );
+//
+//        }
+//    }
+
+
+    public static class IntSumReducer
+            extends Reducer<Text, FloatWritable, Text, Text> {
 
         public void reduce(Text key, Iterable<FloatWritable> byteValues,
                            Context context
         ) throws IOException, InterruptedException {
 
-            MapWritable outputMapRecord = new MapWritable();
             float totalBytes = 0;
+            float averageBytes;
             int counter = 0;
-            double avarageBytes;
 
             for (FloatWritable bytes : byteValues) {
-                totalBytes = totalBytes + Float.parseFloat(bytes.toString());
+                totalBytes += Float.valueOf(bytes.toString());
                 counter++;
             }
-
-            avarageBytes = (double) totalBytes / (double) counter;
-
-            outputMapRecord.put(new Text("total"), new DoubleWritable(totalBytes));
-            outputMapRecord.put(new Text("average"), new DoubleWritable(avarageBytes));
-
-            context.write(key, outputMapRecord );
-
-        }
-    }
-
-
-
-    public static class IntSumReducer
-            extends Reducer<Text, MapWritable, Text, Text> {
-
-        public void reduce(Text key, Iterable<MapWritable> byteValues,
-                           Context context
-        ) throws IOException, InterruptedException {
-
-            double totalBytes = 0;
-            int counter = 0;
-            double averageBytes;
-
-            for (MapWritable bytes : byteValues) {
-                totalBytes = totalBytes + Double.parseDouble(bytes.get(new Text("total")).toString());
-                counter++;
-            }
-
-            averageBytes = (double) totalBytes/(double) counter;
+            averageBytes = (float) totalBytes/(float) counter;
 
             context.write(key, new Text("Total:" + String.valueOf(totalBytes) + " Average:" + String.valueOf(averageBytes)));
-
         }
     }
 
@@ -116,36 +111,16 @@ public class TrafficAnalyzer {
         Job job = Job.getInstance(conf, "traffic analyzer");
         job.setJarByClass(TrafficAnalyzer.class);
         job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(Combiner.class);
+        //job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        job.setMapOutputValueClass(MapWritable.class);
-        job.setSortComparatorClass(IntComparator.class);
+        job.setMapOutputValueClass(FloatWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
 
     }
-
-
-    public static class IntComparator extends WritableComparator {
-
-        public IntComparator() {
-            super(IntWritable.class);
-        }
-
-        @Override
-        public int compare(byte[] b1, int s1, int l1,
-                           byte[] b2, int s2, int l2) {
-
-            Integer v1 = ByteBuffer.wrap(b1, s1, l1).getInt();
-            Integer v2 = ByteBuffer.wrap(b2, s2, l2).getInt();
-
-            return v1.compareTo(v2) * (-1);
-        }
-    }
-
 
 
 }
--- module1_homework2/MR_code/src/test/java/training/bigdata/epam/TrafficAnalyzerTest.java
+++ module1_homework2/MR_code/src/test/java/training/bigdata/epam/TrafficAnalyzerTest.java
@@ -13,19 +13,23 @@ import static org.junit.Assert.assertEquals;
 
 
 public class TrafficAnalyzerTest {
-            MapDriver<Object, Text, Text, FloatWritable> mapDriver;
-            ReduceDriver<Text, MapWritable, Text, Text> reduceDriver;
-            MapReduceDriver<Object, Text, Text, MapWritable, Text, Text> mapReduceDriver;
 
+
+        MapDriver<Object, Text, Text, FloatWritable> mapDriver;
+        ReduceDriver<Text, FloatWritable, Text, Text> reduceDriver;
+        MapReduceDriver<Object, Text, Text, FloatWritable, Text, Text> mapReduceDriver;
 
         @Before
         public void setUp() {
+
             TrafficAnalyzer.TokenizerMapper mapper = new TrafficAnalyzer.TokenizerMapper();
-            TrafficAnalyzer.Combiner combiner = new TrafficAnalyzer.Combiner();
+            //TrafficAnalyzer.Combiner combiner = new TrafficAnalyzer.Combiner();
             TrafficAnalyzer.IntSumReducer reducer = new TrafficAnalyzer.IntSumReducer();
+
             mapDriver = MapDriver.newMapDriver(mapper);
             reduceDriver = ReduceDriver.newReduceDriver(reducer);
-            mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer, combiner);
+            mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer);
+
         }
 
 
@@ -34,18 +38,11 @@ public class TrafficAnalyzerTest {
 
             mapDriver.withInput(new Text("key"), new Text("ip140 - - [24/Apr/2011:12:34:53 -0400] \"GET /sunFAQ/ HTTP/1.1\" 200 8342 \"http://host2/\" \"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16 ( .NET CLR 3.5.30729)\""));
 
-            List<Pair<Text, MapWritable>> results = new ArrayList<Pair<Text, MapWritable>>();
+            List<Pair<Text, FloatWritable>> results = new ArrayList<Pair<Text, FloatWritable>>();
             results = mapDriver.run();
 
-            Iterator<Map.Entry<Writable, Writable>> entryIterator = results.get(0).getSecond().entrySet().iterator();
-
-            Map.Entry<Writable, Writable> entryAverage = entryIterator.next();
-            assertEquals (new Text("average"), (Text) entryAverage.getKey());
-            assertEquals (new DoubleWritable(8342), entryAverage.getValue());
-
-            Map.Entry<Writable, Writable> entryTotal = entryIterator.next();
-            assertEquals (new Text("total"),  entryTotal.getKey());
-            assertEquals (new DoubleWritable(8342), entryTotal.getValue());
+            assertEquals (new Text("ip140"), results.get(0).getFirst());
+            assertEquals (new FloatWritable(8342), results.get(0).getSecond());
 
         }
 
@@ -53,18 +50,10 @@ public class TrafficAnalyzerTest {
         @Test
         public void testReducer() throws IOException {
 
-            List<MapWritable> values = new ArrayList<MapWritable>();
+            List<FloatWritable> values = new ArrayList<FloatWritable>();
 
-            MapWritable mapRecord1 = new MapWritable();
-            mapRecord1.put(new Text("total"), new DoubleWritable(8342));
-            mapRecord1.put(new Text("average"), new DoubleWritable(8342));
-
-            MapWritable mapRecord2 = new MapWritable();
-            mapRecord2.put(new Text("total"), new DoubleWritable(29554));
-            mapRecord2.put(new Text("average"), new DoubleWritable(29554));
-
-            values.add(mapRecord1);
-            values.add(mapRecord2);
+            values.add(new FloatWritable(8342));
+            values.add(new FloatWritable(29554));
 
             reduceDriver.withInput(new Text("ip140"), values);
 
